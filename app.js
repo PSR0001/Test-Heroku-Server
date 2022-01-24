9846bf226e06bbbf507a35661426ec5caa40243e
Partha Singha Roy <parthasingharoy166@gmail.com>
2022-01-24 11:30:24 +0530

    make it better

---
+++ app.js
@@ -0,0 +1,13 @@
+const app = require('express')();
+
+const PORT = 3000;
+
+app.get("/",(req,res)=>{
+    res.send("hello World !")
+});
+
+app.listen(PORT,()=>{
+    console.log(`Server is listening at port ${PORT}`)
+})
+
+
