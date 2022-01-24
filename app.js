94838728cdddd331f3230a3daa4832349e0a791d
Partha Singha Roy <parthasingharoy166@gmail.com>
2022-01-24 13:26:03 +0530

    all set try

--- app.js
+++ app.js
@@ -1,13 +1,61 @@
-const app = require('express')();
-
-const PORT =  process.env.PORT  ||3000;
-
-app.get("/",(req,res)=>{
-    res.send("hi Dipanwita Ki kor6is ree 😁😁")
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const http = require('http');
+const { instrument } = require("@socket.io/admin-ui");
+const favicon = require('serve-favicon')
+const path = require('path')
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server, {
+  cors: {
+    origin: ["https://admin.socket.io"],
+    credentials: true
+  },
 });
 
-app.listen(PORT,()=>{
-    console.log(`Server is listening at port ${PORT}`)
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3000
+
+app.use(favicon(path.join(__dirname, 'views', 'assets/icon/favicon.ico')))
+
+app.set('view engine', 'pug')
+
+
+app.use(cors())
+app.set('views', path.join(__dirname, 'views'))
+
+app.use('/home', require('./routes/homepage'))
+app.use('*', (req, res) => {
+  res.render('webpages/404')
 })
 
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
 
+let message = {
+  "distance": 12,
+  "time": 1
+}
+
+
+try {
+
+  io.on("connection", (socket) => {
+
+    console.log(`User with id: ${socket.id} connected!`);
+
+    socket.on("disconnect", () => {
+      console.log(`User with id: ${socket.id} disconnected`);
+    });
+
+    io.emit("Chart-Data",message)
+
+  });
+
+  instrument(io, { auth: false });
+} catch (error) {
+  console.log(`Could not start the server, ${error}`);
+}
---
+++ views/assets/JS/script.js
@@ -0,0 +1,71 @@
+const URL = "http://localhost:5000/";
+
+// Socket.io Code-->
+const socket = io(URL);
+
+// client-side
+ socket.on("connection", (data) => {
+     console.log(socket.id); // x8WIv7-mJelg7on_ALbx
+     console.log(data);
+
+ });
+
+//  let Data_Distance;
+ //Chart- Data
+ socket.on("Chart-Data", (data) => {
+    //  console.log(data.distance);
+    //  Data_Distance=data;
+    // console.log(Data_Distance)
+    pushValueChart(data)
+ });
+ 
+ socket.on("disconnect", () => {
+     console.log('Socket Disconnected ... ');
+ });
+
+
+/*---Chart JS code---*/
+
+    const ctx = document.getElementById('chart').getContext('2d');
+    const myChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{  //index[0]
+          label: 'Distance',
+          data: [10, 19, 3, 5, 2, 3],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+          ],
+          borderWidth: 1,
+          spanGaps:true,//in case of empty Data
+          showLine:true
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+    
+
+// pushing the incoming Value
+  function pushValueChart(data){
+      // console.log(myChart.data.datasets[0].data)
+      myChart.data.datasets[0].data.push(data.distance)
+      // console.log(myChart.data.datasets[0].data)
+      myChart.data.labels.push(data.time)
+
+      myChart.update();
+  }
+
+
+
+
+ 
